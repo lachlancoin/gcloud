de82d2d459ebc851fff02f53769d58ecda43e589
Lachlan Coin <ljmcoinwork@gmail.com>
2019-03-03 16:37:00 +1000

    updates

--- shutdown.sh
+++ shutdown.sh
@@ -4,10 +4,6 @@ if [ ! $ALIGNER_REGION ]; then
 	echo "please define global parameter ALIGNER_REGION using e.g. export ALIGNER_REGION=\"asia-northeast1\""
 	exit 1;
 fi
-if [ ! $PROVISION_SCRIPT ]; then
-	echo "please define PROVISION_SCRIPT"
-	exit 1;
-fi
 
 if [ ! $UPLOAD_SUBSCRIPTION ]; then
 	echo "please define UPLOAD_SUBSCRIPTION"
@@ -15,8 +11,11 @@ if [ ! $UPLOAD_SUBSCRIPTION ]; then
 fi
 
 gcloud dataflow jobs list | grep Running | cut -f 1 -d ' ' | xargs -I {} gcloud dataflow jobs --project=nano-stream1 cancel --region=$ALIGNER_REGION {}
-cd ./gcloud/aligner
-bash ./${PROVISION_SCRIPT} -c
+
+#DE-COMMISSION ALIGNER
+source ./gcloud/aligner/provision_internal.sh
+cleanup
+
 
 #CLOSE SUBSCRIPTION
 echo "gcloud pubsub subscriptions list | grep name | grep ${UPLOAD_SUBSCRIPTION} | cut -f 2 -d ' '  | xargs -I {} gcloud pubsub subscriptions delete {}"
